ff283f0fb51c7f9efd92a1b6725338e05650e47c
mayurigit123 <mayuripatil10244@gmail.com>
2022-02-13 19:29:57 +0530

    Fifth Commit

---
+++ demo.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=eval(input('Enter the no: '))
+print(a)
+
+
+# In[5]:
+
+
+a=eval(input('Enter the no: '))
+for x in range (10):
+    print((x+1)*a)
+
+
+# In[6]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[7]:
+
+
+get_ipython().run_line_magic('lsmagic', '')
+
+
+# In[8]:
+
+
+get_ipython().run_cell_magic('HTML', '', '<iframe width="420" height="315"\nsrc="https://www.youtube.com/embed/tgbNymZ7vqY">\n</iframe>')
+
+
+# In[10]:
+
+
+print("Hello Demo")
+
+
+# In[ ]:
+
+
+
+
