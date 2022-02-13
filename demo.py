1ada4c4441ca4dfa46faa5e23b032ea8358b3fc3
mayurigit123 <mayuripatil10244@gmail.com>
2022-02-13 19:48:44 +0530

    Sixth Commit

--- demo.py
+++ demo.py
@@ -8,40 +8,18 @@ a=eval(input('Enter the no: '))
 print(a)
 
 
-# In[5]:
+# In[16]:
 
 
 a=eval(input('Enter the no: '))
-for x in range (10):
-    print((x+1)*a)
-
-
-# In[6]:
-
-
-get_ipython().run_line_magic('pwd', '')
-
-
-# In[7]:
-
-
-get_ipython().run_line_magic('lsmagic', '')
-
-
-# In[8]:
-
-
-get_ipython().run_cell_magic('HTML', '', '<iframe width="420" height="315"\nsrc="https://www.youtube.com/embed/tgbNymZ7vqY">\n</iframe>')
-
-
-# In[10]:
-
-
-print("Hello Demo")
-
-
-# In[ ]:
-
-
-
+s=0
+for x in range (2,a):
+    if(a%x==0):
+        s=1
+        
+if(s==1):
+    print("Not Prime")
+else:
+    print("Prime")
+    
 
